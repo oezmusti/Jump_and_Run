36ad064dc854b359ab902801f0886b623b87114b
Mustafa Öz <mustafa2002oezmail.com>
2023-12-22 15:09:55 +0100

    Einfügen vonder Klasse Object und start der Auslagerung von dem Spieler in eine eigene Klasse

---
+++ src/Objects.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+//Nutzung der Klasse als Interface für jegliche Objekte in dem Spiel
+public class Objects {
+    public Image object;
+
+    public void paint(){
+
+    }
+}
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Player {
+    public Image playerImg;
+
+    public void setPlayerImg(){
+        ImageIcon player = new ImageIcon("src\\assets\\Charakter-stehend.png");
+        playerImg = player.getImage();
+    }
 }
