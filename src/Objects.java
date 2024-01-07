cbef5883e64de9e4b9add2c0101926a4aa6ac439
Mustafa Öz <mustafa2002oezmail.com>
2024-01-07 16:14:25 +0100

    Changes by janar

--- src/Objects.java
+++ src/Objects.java
@@ -5,8 +5,12 @@ import java.awt.*;
 //Nutzung der Klasse als Interface für jegliche Objekte in dem Spiel
 public abstract class Objects {
     protected Image object;
+    // Was geeeeeht!!!!!
 
+    protected String name;
+    
     public void paint(){}
 
     public Objects(){}
 }
+
